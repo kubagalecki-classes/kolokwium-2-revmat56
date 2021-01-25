84de6bea762e96e1ebb749c35f1d33ea1e5cd042
revmat56 <62846638+revmat56@users.noreply.github.com>
2021-01-25 19:15:11 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -14,7 +14,7 @@ void jedzOstatnieWarzywa(const std::vector< Warzywo > w, unsigned n, std::ostrea
         throw std::logic_error("a");
     }
 
-    for (auto it = w.crbegin() - 1; it != w.crend() + 1; ++it) {
+    for (auto it = w.rbegin() - 1; it != w.rend() + 1; ++it) {
         it -> opis(s);
     }
 }
