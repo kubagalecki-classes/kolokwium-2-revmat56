4c01e75b8fa2bfc33d8c7af56f1f06a5d5cc9527
revmat56 <62846638+revmat56@users.noreply.github.com>
2021-01-25 17:43:21 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,37 @@
 #include <string>
 
 // tutaj klasa Warzywo
+
+class Warzywo{
+public:
+    Warzywo(const std::string& a, double b, int c): nazwa(a), cena(b), kolor(c)
+    { 
+        i++;
+    }
+    ~Warzywo() 
+    {
+        i--;
+    }
+
+    static unsigned getVeg() 
+    {
+        return i;
+    }
+
+    void opis(std::ostream& s) const 
+    {
+        s << "[" << nazwa << "]"
+          << ":"
+          << "[" << cena << "]"
+          << ","
+          << "[" << kolor << "]" << std::endl;
+    }
+
+private:
+    std::string nazwa;
+    double      cena;
+    int         kolor;
+    inline static unsigned i;
+
+};
+inline unsigned Warzywo::i = 0;
